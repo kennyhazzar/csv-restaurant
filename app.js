a920447a820caadfe5901787305a20d597804d5d
kolchanov <kolchanov@siberian.pro>
2021-05-13 06:37:32 +0000

    some edit

---
+++ app.js
@@ -0,0 +1,17 @@
+﻿import express from 'express'
+import generate from './routes/api/generate.js'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+let app = express()
+app.use(express.static(__dirname))
+app.use('/api', generate)
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html')
+  })
+
+app.listen(3000, () => console.log("i'm started"))
---
+++ routes/api/generate.js
@@ -0,0 +1,25 @@
+﻿import csv from '@fast-csv/parse'
+import { Router } from 'express'
+import fs from 'fs'
+import multer from 'multer'
+import path from 'path'
+import { fileURLToPath } from 'url'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+const router = Router()
+router.use(multer({ dest: "routes/api" }).single("filedata"))
+router.post('/generate', (req, res, next) => {
+    const filedata = req.file
+    console.log(filedata)
+    fs.rename(`./routes/api/${filedata.filename}`, `./routes/api/${filedata.originalname}`, err => {
+        if (err) throw err
+        console.log('renamed complete')
+    })
+    res.redirect('/')
+
+})
+
+
+export default router
