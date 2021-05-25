396058421233ab05cab614a6ec04703b7dc850e1
kolchanov <kolchanov@siberian.pro>
2021-05-25 05:11:50 +0000

    Изменение некоторых спецификаций модели Меню в базе данных

--- model/Menu.js
+++ model/Menu.js
@@ -1,12 +1,12 @@
 ﻿const { Schema, model } = require('mongoose')
 
 const schema = new Schema({
-    title: {type: String, required: true, default: ""},
+    title: { type: String, required: true, default: "" },
     date: { type: Date, required: true, default: Date.now },
     views: { type: Number, default: 0 },
     codeMenu: { type: String, unique: true, required: true },
     sheetArray: { type: Array, unique: false, required: true },
-    menuUri: {type: String, unique: true, required: true},
+    menuUri: { type: String, unique: true, required: true },
 })
 
 module.exports = model('Menu', schema)
