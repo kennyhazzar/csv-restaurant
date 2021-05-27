4c13338e6e1e42ecba759f234b2a2574789ee7b9
kolchanov <kolchanov@siberian.pro>
2021-05-27 14:55:13 +0000

    Обернул навигационную панель в класс navbar-fixed

--- client/src/Components/Navbar/Navbar.js
+++ client/src/Components/Navbar/Navbar.js
@@ -2,15 +2,18 @@
 import { NavLink } from 'react-router-dom'
 export const Navbar = () => {
     return (
-        <nav>
-            <div className="nav-wrapper green darken-1" style={{ padding: '0 2rem' }}>
-                <span className="brand-logo">csv-restaurant</span>
-                <ul id="nav-mobile" className="right hide-on-med-and-down">
-                    <li><NavLink to={`/generate`}>Создать</NavLink></li>
-                    <li><NavLink to={`/menu`}>Последние добавленные</NavLink></li>
-                    <li><NavLink to={`/rules`}>Как пользоваться</NavLink></li>
-                </ul>
-            </div>
-        </nav>
+        //  style={{ display: 'inline-flex' }}
+        <div className="navbar-fixed">
+            <nav>
+                <div className="nav-wrapper green darken-1" style={{ padding: '0 2rem', }}>
+                    <span className="brand-logo">csv-restaurant</span>
+                    <ul id="nav-mobile" className="right hide-on-med-and-down">
+                        <li><NavLink to={`/generate`}>Создать</NavLink></li>
+                        <li><NavLink to={`/menu`}>Последние добавленные</NavLink></li>
+                        <li><NavLink to={`/rules`}>Как пользоваться</NavLink></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
     )
 }
